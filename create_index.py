487cf2e7cf4457a670546e2eca6293dab2ea78cb
SaisivaNanda <118908512+SaisivaNanda@users.noreply.github.com>
2026-02-28 01:50:11 +0530

    Change API_KEY to placeholder in create_index.py
    Updated API_KEY placeholder for security reasons.

--- create_index.py
+++ create_index.py
@@ -1,7 +1,7 @@
 from elasticsearch import Elasticsearch
 
 ELASTIC_ENDPOINT = "https://my-elasticsearch-project-ca16ca.es.asia-south1.gcp.elastic.cloud:443"
-API_KEY = "ZVJUVGxad0JycnlScjdUS0hwblg6RlJucWhuaVRCejJGRUIzQzhla3pMQQ=="
+API_KEY = "write your elastic api key here"
 
 es = Elasticsearch(
     ELASTIC_ENDPOINT,
