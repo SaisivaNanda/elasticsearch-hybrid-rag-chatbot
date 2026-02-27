9ad12e8c081aa37daa975ecbe35f7ff0701c55ee
SaisivaNanda <118908512+SaisivaNanda@users.noreply.github.com>
2026-02-28 01:50:34 +0530

    Change API_KEY to a placeholder
    Updated API_KEY placeholder for security reasons.

--- index_documents.py
+++ index_documents.py
@@ -5,7 +5,7 @@ from sentence_transformers import SentenceTransformer
 # CONFIG
 
 ELASTIC_ENDPOINT = "https://my-elasticsearch-project-ca16ca.es.asia-south1.gcp.elastic.cloud:443"
-API_KEY = "ZVJUVGxad0JycnlScjdUS0hwblg6RlJucWhuaVRCejJGRUIzQzhla3pMQQ=="
+API_KEY = "write your elastic api key here"
 INDEX_NAME = "chatbot-docs"
 
 
